ab2eb9e96b820fb940ac43e206449d287b19bcc0
gbrisc <gabrisc427@gmail.com>
2025-06-06 20:06:01 +0200

    CAMBIO DE ESTRUCTURA DEL PROYECTO (GENERACIÓN SPRING BOOT)

---
+++ src/main/java/com/money_manager/MoneyManagerWebappApplication.java
@@ -0,0 +1,13 @@
+package com.money.manager.webapp.money_manager_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagerWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagerWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/money_manager/MoneyManagerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.money.manager.webapp.money_manager_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
