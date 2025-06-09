4429356dedba1c556575cb0cd86976ee4cfa4cf9
gbrisc <gabrisc427@gmail.com>
2025-06-09 16:39:41 +0200

    Pequeños cambios

--- money-manager-frontend/src/App.tsx
+++ money-manager-frontend/src/App.tsx
@@ -4,32 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mensaje, setMensaje] = useState('');
 
-  return (
-    <>
+    useEffect(() => {
+      fetch('http://localhost:8081/api/hello')
+        .then(res => res.text())
+        .then(data => setMensaje(data))
+        .catch(err => console.error('Error al llamar al backend:', err));
+    }, []);
+
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Frontend React</h1>
+        <p>{mensaje}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    );
+  }
+
 
 export default App
